1883f274cc3167925d61f250b1f89a020b915dbe
shahbazahmad4515-ai <shahbazahmad4515@gmail.com>
2026-02-24 22:11:54 +0500

    Update app.py

--- app.py
+++ app.py
@@ -72,9 +72,10 @@ uploaded_file = st.file_uploader("Choose an image...", type=["jpg", "jpeg", "png
 
 if uploaded_file is not None:
     image = Image.open(uploaded_file).convert("RGB")
-    st.image(image, caption='Uploaded Image', use_column_width=True)
+    st.image(image, caption='Uploaded Image',  use_container_width=True)
     
     if st.button('Generate Caption'):
         with st.spinner('AI is thinking...'):
             caption = generate_caption(image, model, vocab, resnet_extractor)
+
             st.success(f"**AI says:** {caption}")
